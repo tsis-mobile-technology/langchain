c1613db56fc74b117a0a0496064a9b30944ab642
taejonggo <gainworld@gmail.com>
2024-01-14 14:16:11 +0900

    code test

--- langchain_llamacpp.py
+++ langchain_llamacpp.py
@@ -26,17 +26,17 @@ from langchain_community.llms import LlamaCpp
 # model_path = expanduser("D:\\Programming\\llama.cpp\\models\\M-SOLAR-10.7B-V2\\ggml-model-q4_k_m.gguf")
 # model_path = expanduser("D:\\Programming\\llama.cpp\\models\\Code-13B-Instruct\\ggml-model-q4_k_m.gguf")
 # model_path = expanduser("D:\\Programming\\llama.cpp\\models\\Code-34B-Instruct\\ggml-model-q4_k_m.gguf")
-model_path = expanduser("D:\\Programming\\llama.cpp\\models\\Code-34B-Python\\ggml-model-q4_k_m.gguf")
-n_batch = 128
-max_new_tokens = 512
-temperature = 0.7
+# model_path = expanduser("D:\\Programming\\llama.cpp\\models\\M-WIZARD-13B\\ggml-model-q4_k_m.gguf")
+# model_path = expanduser("D:\\Programming\\llama.cpp\\models\\Code-34B-Python\\ggml-model-q4_k_m.gguf")
+# model_path = expanduser("D:\\Programming\\llama.cpp\\models\\Code-13B-Python\\ggml-model-q4_k_m.gguf")
+# model_path = expanduser("D:\\Programming\\llama.cpp\\models\\C-WIZARD-33B-Python\\ggml-model-q4_k_m.gguf")
+model_path = expanduser("D:\\Programming\\llama.cpp\\models\\16K-Vicuna-13B\\ggml-model-q4_k_m.gguf")
+n_batch = 1024
+max_new_tokens = 2048
+temperature = 0.5
 top_p = 0.95
 top_k = 40
 repetition_penalty = 1.1
-# model_path = expanduser("D:\\Programming\\llama.cpp\\models\\M-WIZARD-13B\\ggml-model-q4_k_m.gguf")
-# model_path = expanduser("D:\\Programming\\llama.cpp\\models\\C-WIZARD-33B-Python\\ggml-model-q4_k_m.gguf")
-# model_path = expanduser("D:\\Programming\\llama.cpp\\models\\16K-Vicuna-13B\\ggml-model-q4_k_m.gguf")
-
 callback_manager: CallbackManager = CallbackManager([StreamingStdOutCallbackHandler()])
 n_gpu_layers = 40
 # Change this value based on your model and your GPU VRAM pool.
@@ -47,7 +47,7 @@ llm = LlamaCpp(
     model_path=model_path,
     temp=temperature,
     max_new_tokens=max_new_tokens,
-    ctx_size=4096,
+    ctx_size=30000,
     top_k=top_k,
     top_p=top_p,
     repeat_penalty=repetition_penalty,
@@ -60,43 +60,54 @@ model = Llama2Chat(llm=llm)
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 chain = LLMChain(llm=model, prompt=prompt_template, memory=memory)
 
+## Vicuna Model test prompt(16k)
 print(
-    chain.run("""### System Prompt
-              Write code to solve the following coding problem that obeys the constraints and passes the example test cases. Please wrap your code answer using ```:
-        ### User Message
-              In Python, how do I list all text files in the current directory (excluding subdirectories) that have been modified in the last month
-        ### Assistant
-        """
-    )
-)
-print(
-    chain.run(text="""### System Prompt
-        ### User Message
-              Tell me more about other code.
-        ### Assistant
-        """
+    chain.run(
+        text = """A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
+USER: Hello!
+ASSISTANT: Hello!</s>
+USER: How are you?
+ASSISTANT: I am good.</s>
+"""
     )
 )
 
+## Python Code Model test prompt(LLAMA 2 LLM)
 # print(
 #     chain.run(
 #         text="""[INST] Write code to solve the following coding problem that obeys the constraints and passes the example test cases. Please wrap your code answer using ```:
-#         In Bash, how do I list all text files in the current directory (excluding subdirectories) that have been modified in the last month
+#         I have a Python class that would benefit from a binary sorting algorithm. Could you please create a Python class that takes in a list of values, sorts them using binary sort, and returns the sorted list. 
 #         [/INST]"""
 #     )
 # )
-# print(chain.run(text="[INST]\nTell me more about other code.[\INST]\n\n"))
 
+## Python Code Model test prompt(WIZARD LLM)
 # print(
-#     chain.run(
-#         text="[INST]\nWhat can I see in Vienna? Propose a few locations. Names only, no details.[\INST]\n\n"
+#     chain.run("""### System Prompt
+#               Write code to solve the following coding problem that obeys the constraints and passes the example test cases. Please wrap your code answer using ```:
+#         ### User Message
+#               I have a Python class that would benefit from a binary sorting algorithm. Could you please create a Python class that takes in a list of values, sorts them using binary sort, and returns the sorted list. 
+#         ### Assistant
+#         """
 #     )
 # )
-# print(chain.run(text="[INST]\nTell me more about #2.[\INST]\n\n"))
 
+## Math Model test prompt
 # print(
-#     chain.run(
-#         text="대한민국에 있는 유명한 산 이름을 10개 정도 나열해주세요."
+#     chain.run(text="""### System Prompt
+#         ### User Message
+#                Fifty people and 50 lamps were numbered from 1 to 50. All of these lights are turned on by the number 1, and those with number 2 are turned off by numbers in multiples of 2. If you are number n, look at the light with the number of the multiple of n and turn it off if it's on, and turn it on if it's off. After all 50 people have done this one after the other, ask how many lights are on.
+#         ### Assistant
+#         """
 #     )
 # )
-# print(chain.run(text="Tell me more about #2."))
+
+# print(
+#     chain.run("""### System Prompt
+#               Write code to solve the following coding problem that obeys the constraints and passes the example test cases. Please wrap your code answer using ```:
+#         ### User Message
+#               In Python, how do I list all text files in the current directory (excluding subdirectories) that have been modified in the last month
+#         ### Assistant
+#         """
+#     )
+# )
